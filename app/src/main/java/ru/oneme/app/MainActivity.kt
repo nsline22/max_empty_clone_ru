61bf8b7b2dda473e326c8d64d9b228aefa380180
nsline22 <vdmkulagin@gmail.com>
2025-08-06 11:22:32 +0300

    Update.

---
+++ app/src/main/java/ru/oneme/app/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.oneme.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.oneme.app.ui.theme.MAXTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish() // Просто закрываем активность и приложение
+        return
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MAXTheme {
+        Greeting("Android")
+    }
+}
